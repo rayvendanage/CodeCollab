2518665176b3f1afab0eea061c3eb75e5e0b952e
Rayven Danage <69762401+rayvendanage@users.noreply.github.com>
2023-11-01 08:12:32 -0400

    remove unused import

--- app/api/favorites/[projectId]/route.ts
+++ app/api/favorites/[projectId]/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 
 import prisma from "@/app/libs/prismadb";
 import getCurrentUser from "@/app/actions/getCurrentUser";
-import { NormalizeError } from "next/dist/shared/lib/utils";
 
 type Iparams = {
     projectId?: string;
